e99067de8020ab32dde9d13c4a225ad4f7adee2c
Pranav Makote <80123831+pranavmak@users.noreply.github.com>
2024-07-20 23:40:08 +0530

    Create daily_use_script.sh

---
+++ daily_use_script.sh
@@ -0,0 +1,14 @@
+#list all files under /etc which modified in last 30 days
+find /etc -iname "8" -type f -mtime -30
+
+#to see root word in log file
+grep "root" /etc/log/new.log
+
+#to see how many time root word came 
+grep -c "root" /etc/log/new.log
+
+# sed cmd - stream editor for substitute word
+sed -i "s/Test/Hello/" devops.txt
+
+# to print second column and first 100 lines
+awk '{print $2}' /var/log/dnf.log | head -100
